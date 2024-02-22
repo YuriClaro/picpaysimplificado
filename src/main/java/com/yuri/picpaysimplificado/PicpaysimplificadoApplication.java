4e79ee6a82f57edbaab82782d79bcbc9d925e360
Yuri Claro <yuri.claro@outlook.com>
2024-02-22 15:47:28 -0300

    inicio do projeto

---
+++ src/main/java/com/yuri/picpaysimplificado/PicpaysimplificadoApplication.java
@@ -0,0 +1,13 @@
+package com.yuri.picpaysimplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaysimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaysimplificadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuri/picpaysimplificado/PicpaysimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuri.picpaysimplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaysimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

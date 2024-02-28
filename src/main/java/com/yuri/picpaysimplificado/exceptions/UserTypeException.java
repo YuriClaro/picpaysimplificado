1eec220ebe27c791869578ea6f7ab3faf578751b
Yuri Claro <yuri.claro@outlook.com>
2024-02-28 12:10:45 -0300

    feat: add UserTypeException

---
+++ src/main/java/com/yuri/picpaysimplificado/exceptions/UserTypeException.java
@@ -0,0 +1,8 @@
+package com.yuri.picpaysimplificado.exceptions;
+
+public class UserTypeException extends Exception{
+
+    public UserTypeException(String message) {
+        super(message);
+    }
+}

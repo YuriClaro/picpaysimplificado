620dd9c77c30d7e19d3e8e4dfd03be5f8d54fb7c
Yuri Claro <yuri.claro@outlook.com>
2024-02-28 12:10:54 -0300

    feat: add SaldoInsuficienteException

---
+++ src/main/java/com/yuri/picpaysimplificado/exceptions/SaldoInsuficienteException.java
@@ -0,0 +1,8 @@
+package com.yuri.picpaysimplificado.exceptions;
+
+public class SaldoInsuficienteException extends Exception {
+
+    public SaldoInsuficienteException(String message) {
+        super(message);
+    }
+}
